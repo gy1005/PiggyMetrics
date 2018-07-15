e6344ad30e19919533cd239e329bd22fe3a6d802
Yu Gan <yg397@cornell.edu>
2018-07-14 21:31:35 -0700

    Add distributed tracing to Zipkin

--- gateway/src/main/java/com/piggymetrics/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/piggymetrics/gateway/GatewayApplication.java
@@ -1,9 +1,12 @@
 package com.piggymetrics.gateway;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,5 +15,10 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public AlwaysSampler defaultSampler() {
+		return new AlwaysSampler();
 	}
 }
